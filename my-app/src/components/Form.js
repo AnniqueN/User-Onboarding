2860c96af908453022f16cbf3f6c40cd68de97a2
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-07 19:22:17 -0500

    Made few changes and styling on the form

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,109 +1,120 @@
-import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { withFormik, Form as FormikForm, Field } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
 
-function TheForm({ values, errors, touched, isSubmitting, status, form }) {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    // status comes through as undefined sometimes
-    if (status) {
-      setUsers([...users, status]);
-    }
-  }, [status]);
+const Form = ({ errors, touched, status, setUsers, values }) => {
+  useEffect( () =>
+    {
+        if(status)
+        {
+            setUsers(users => [...users, status])
+        }
+    }, [status])
   return (
-    <>
-      <Form>
+    <div>
+      <h1>User Form</h1>
+      <FormikForm className='user-form'>
         <label>
-          Enter Name
+          Username:
+          <Field type='text' name='username' />
+          {touched.username && errors.username && (
+            <p className='error'>{errors.username}</p>
+          )}
+        </label>
+        <label>
+          Name:
+          <Field type='text' name='name' />
           {touched.name && errors.name && (
-            <p className="errors">{errors.name}</p>
+            <p className='error'>{errors.name}</p>
           )}
-          <Field type="text" name="name" placeholder="Full Name" />
         </label>
         <label>
-          Enter Email
-          {touched.email && errors.email && (
-            <p className="errors">{errors.email}</p>
-          )}
-          <Field type="text" name="email" placeholder="Email" />
-        </label>
-        <label>
-          Enter Password
-          {touched.password && errors.password && (
-            <p className="errors">{errors.password}</p>
-          )}
-          <Field type="password" name="password" placeholder="Password" />
-        </label>
-        <label>
-          Pick Role
-          <Field component="select" name="role" className="selectOption">
-            <option value="newbie">Newbie</option>
-            <option value="starter">Starter</option>
-            <option value="basic">Basic</option>
-            <option value="expert">Expert</option>
+          Role:
+          <Field component='select' name='role'>
+            <option>Select a Role</option>
+            <option value='Newbie'>Newbie</option>
+            <option value='Starter'>Starter</option>
+            <option value='Intermediate'>Intermediate</option>
+            <option value='Expert'>Expert</option>
           </Field>
+          {touched.role && errors.role && (
+            <p className='error'>{errors.role}</p>
+          )}
         </label>
         <label>
-          Terms of Service
-          <Field type="checkbox" name="terms" checked={values.terms} />
+          Email:
+          <Field type='email' name='email' />
+          {touched.email && errors.email && (
+            <p className='error'>{errors.email}</p>
+          )}
         </label>
-        <button type="submit">Submit</button>
-      </Form>
-      <h1>Current Users</h1>
-      {users
-        ? users.map(user => (
-            <p key={user.id} className="users">
-              Name : {user.name} Role : {user.role}
-            </p>
-          ))
-        : null}
-    </>
+        <label>
+          Password:
+          <Field type='password' name='password' />
+          {touched.password && errors.password && (
+            <p className='error'>{errors.password}</p>
+          )}
+        </label>
+        <label>
+          Terms of Service:
+          <Field
+            type='checkbox'
+            name='termOfService'
+            checked={values.termOfService}
+          />
+          {touched.termOfService && errors.termOfService && (
+            <p className='error'>{errors.termOfService}</p>
+          )}
+        </label>
+        <button type='submit' className='user-form-submit'>
+          Submit
+        </button>
+      </FormikForm>
+    </div>
   );
-}
+};
 
-const FormikForm = withFormik({
-  //Handle the inputs using the args you give to you mapPropsToValues
-
-  enableReinitialize: true,
-  mapPropsToValues: ({ name, email, password, terms, users, role }) => {
+const FormikUserForm = withFormik({
+  mapPropsToValues(values) {
     return {
-      name: "",
-      email: "",
-      password: "",
-      terms: false,
-      role: "noob",
-      users: ["Example User"]
+      username: values.username || '',
+      name: values.name || '',
+      role: values.role || '',
+      email: values.email || '',
+      password: values.password || '',
+      termOfService: values.termOfService || false,
     };
   },
+  //VALIDATION SCHEMA 
   validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .lowercase()
+      .required('Username is a required field'),
     name: Yup.string()
-      .min(4, "Please Enter Your First and Last Name")
-      .required("Please Enter Your Name"),
+      .lowercase()
+      .required('Name is a required field'),
+    role: Yup.string().required('Role is a required field'),
     email: Yup.string()
-      .email()
-      .required("Please Enter Your Email"),
+      .lowercase()
+      .email('Field needs to be a valid e-mail')
+      .required('E-mail is a required field'),
     password: Yup.string()
-      .min(6, "Password must be at least 5 characters with 1 character")
-      .required("Please Enter Your Password")
+      .min(8, 'Password must be at least 6 characters and 1 number')
+      .required('Password is a required field'),
+    termOfService: Yup.bool()
+      .oneOf([true], 'Terms of Service must be checked')
+      .required(),
   }),
-  handleSubmit(values, { resetForm, setStatus }) {
-    axios
-      .post("https://reqres.in/api/users", {
-        name: values.name,
-        email: values.email,
-        password: values.password,
-        terms: values.terms,
-        role: values.role
-      })
-      .then(response => {
-        setStatus(response.data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    resetForm();
-  }
-})(TheForm);
+  //END OF VALIDATION SCHEMA
+  handleSubmit(values, { setStatus, resetForm }) {
+    // console.log(values);
+    axios.post('https://reqres.in/api/users', values).then(res => {
+      console.log('HTTP POST response: ', res);
+      setStatus(res.data);
+      resetForm();
+    });
+  },
+})(Form);
 
-export default FormikForm;
+export default FormikUserForm;
